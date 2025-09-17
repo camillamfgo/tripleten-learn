55be29114cc5e1ae60b05d03cc304ea45dacbc0a
Camilla Oliveira <camilla.mfgo@gmail.com>
2025-09-18 00:02:32 +0100

    Lição 7 do capítulo 6 da sprint 1

--- tripleten.py
+++ tripleten.py
@@ -53,3 +53,63 @@ for rates in ratings:
         print(5)
     else:
          print('Não aplicável')
+
+
+
+'''Tarefa
+Obtenha todos os filmes dos EUA da tabela movies_info usando um filtro. 
+Armazene o resultado na lista movies_filtered. 
+Imprima essa lista quando terminar.'''
+
+movies_info = [
+    ['The Shawshank Redemption', 'USA', 1994, 'drama', 142, 9.111],
+    ['The Godfather', 'USA', 1972, 'drama, crime', 175, 8.730],
+    ['The Dark Knight', 'USA', 2008, 'fantasy, action, thriller', 152, 8.499],
+    ["Schindler's List", 'USA', 1993, 'drama', 195, 8.818],
+    ['The Lord of the Rings: The Return of the King', 'New Zealand', 2003, 'fantasy, adventure, drama', 201, 8.625],
+    ['Pulp Fiction', 'USA', 1994, 'thriller, comedy, crime', 154, 8.619],
+    ['The Good, the Bad and the Ugly', 'Italy', 1966, 'western', 178, 8.521],
+    ['Fight Club', 'USA', 1999, 'thriller, drama, crime', 139, 8.644],
+    ['Harakiri', 'Japan', 1962, 'drama, action, history', 133, 8.106],
+    ['Good Will Hunting', 'USA', 1997, 'drama, romance', 126, 8.077]
+]
+
+movies_filtered = []
+
+for movie in movies_info:
+    if movie[1] == 'USA':
+        movies_filtered.append(movie)
+
+print(movies_filtered)
+
+
+'''Tarefa
+Parece que você está se tornando um especialista em filtragem de filmes! 
+Continue explorando e filtrando filmes para encontrar seus favoritos.
+
+Agora tente por conta própria:
+
+Crie a lista vazia chamada movies_filtered. 
+Em seguida, adicione a movies_filtered filmes que foram lançados em 1994 ou possui uma classificação inferior a 8,5.'''
+
+movies_info = [
+    ['The Shawshank Redemption', 'USA', 1994, 'drama', 142, 9.111],
+    ['The Godfather', 'USA', 1972, 'drama, crime', 175, 8.730],
+    ['The Dark Knight', 'USA', 2008, 'fantasy, action, thriller', 152, 8.499],
+    ["Schindler's List", 'USA', 1993, 'drama', 195, 8.818],
+    ['The Lord of the Rings: The Return of the King', 'New Zealand', 2003, 'fantasy, adventure, drama', 201, 8.625],
+    ['Pulp Fiction', 'USA', 1994, 'thriller, comedy, crime', 154, 8.619],
+    ['The Good, the Bad and the Ugly', 'Italy', 1966, 'western', 178, 8.521],
+    ['Fight Club', 'USA', 1999, 'thriller, drama, crime', 139, 8.644],
+    ['Harakiri', 'Japan', 1962, 'drama, action, history', 133, 8.106],
+    ['Good Will Hunting', 'USA', 1997, 'drama, romance', 126, 8.077]
+]
+
+movies_filtered = []# crie uma lista vazia chamada movies_filtered
+
+for movie in movies_info:
+    if movie[2] == 1994 or movie[5] < 8.5:
+        movies_filtered.append(movie)
+
+for movie in movies_filtered:
+    print(movie) 
