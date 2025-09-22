c796f361821ec5395f39bae533d5f3453820a036
Camilla Oliveira <camilla.mfgo@gmail.com>
2025-09-22 17:14:57 +0100

    lição4_cap2_sprint2

--- Sprint2_Tripleten.py
+++ Sprint2_Tripleten.py
@@ -34,3 +34,16 @@ nike_price = financial_info.get('Nike')# escreva o código aqui
 print(nike_price)
 
 
+financial_info = {
+    'American Express': 93.23,
+    'Boeing': 178.44,
+    'Coca-Cola': 45.15,
+    'Walt Disney': 119.34,
+    'Nike': 97.99,
+    'JPMorgan':96.27,
+    'Walmart': 130.68 
+}
+
+financial_info['Microsoft']=208.35# adicione um novo par aqui
+
+print(financial_info)
