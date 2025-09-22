1073d333e8f2a952be09e994cc9ae0e7c4f17337
Camilla Oliveira <camilla.mfgo@gmail.com>
2025-09-22 17:13:44 +0100

    lição3_cap2_sprint2

--- Sprint2_Tripleten.py
+++ Sprint2_Tripleten.py
@@ -6,3 +6,31 @@ movies = {
 }# crie um dicionário
 
 print(movies)
+
+financial_info = {
+    'American Express': 93.23,
+    'Boeing': 178.44,
+    'Coca-Cola': 45.15,
+    'Walt Disney': 119.34,
+    'Nike': 97.99,
+    'JPMorgan':96.27,
+    'Walmart': 130.68 
+}
+
+walmart_price = financial_info['Walmart']# Escreva seu código aqui
+print(walmart_price)
+
+financial_info = {
+    'American Express': 93.23,
+    'Boeing': 178.44,
+    'Coca-Cola': 45.15,
+    'Walt Disney': 119.34,
+    'Nike': 97.99,
+    'JPMorgan':96.27,
+    'Walmart': 130.68 
+}
+
+nike_price = financial_info.get('Nike')# escreva o código aqui
+print(nike_price)
+
+
